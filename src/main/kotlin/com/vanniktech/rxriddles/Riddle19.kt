8daa8373ecadaafe4e3220b8fe6f3704b7b9faf0
Gulzar <gulzarhome54@gmail.com>
2018-08-21 22:01:19 +0530

    19 - 25 riddles solved

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle19.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle19.kt
@@ -3,17 +3,23 @@ package com.vanniktech.rxriddles
 import io.reactivex.Observable
 
 object Riddle19 {
-  /**
-   * Use the given [Interaction] interface and use its listener to transform the [Int] callback to an Observable that emits every time the listener is called.
-   * When the Observable is being disposed the listener should be set to null.
-   *
-   * Use case: Transform any listener into an Observable.
-   */
-  fun solve(interaction: Interaction): Observable<Int> {
-    TODO()
-  }
+    /**
+     * Use the given [Interaction] interface and use its listener to transform the [Int] callback to an Observable that emits every time the listener is called.
+     * When the Observable is being disposed the listener should be set to null.
+     *
+     * Use case: Transform any listener into an Observable.
+     */
+    fun solve(interaction: Interaction): Observable<Int> {
+        return Observable.create { emi ->
+            interaction.listener = emi::onNext
 
-  interface Interaction {
-    var listener: ((Int) -> Unit)?
-  }
+            emi.setCancellable {
+                interaction.listener = null
+            }
+        }
+    }
+
+    interface Interaction {
+        var listener: ((Int) -> Unit)?
+    }
 }
